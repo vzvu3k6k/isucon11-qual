c41ce07ff13c60ee87ca2858c0a852c9bd85b451
vzvu3k6k <vzvu3k6k@gmail.com>
2021-08-21 12:44:19 +0900

    Remove count from postIsuCondition

--- go/main.go
+++ go/main.go
@@ -1186,7 +1186,7 @@ func postIsuCondition(c echo.Context) error {
 	defer tx.Rollback()
 
 	var count int
-	err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ?", jiaIsuUUID)
+	err = tx.Get(&count, "SELECT 1 FROM `isu` WHERE `jia_isu_uuid` = ? LIMIT 1", jiaIsuUUID)
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
